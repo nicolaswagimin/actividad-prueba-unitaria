e41b48a7ad8786e78c81d349c5e533f278114cd8
nicolaswagimin <nicolas.wagimin@campusucc.edu.co>
2025-04-21 11:49:22 -0500

    Actualización de dependencias y pruebas

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders actividad header', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = screen.getByText(/Actividad: Prueba Unitaria con Jest/i); // Actualiza el texto que se busca
+  expect(headerElement).toBeInTheDocument();
 });
